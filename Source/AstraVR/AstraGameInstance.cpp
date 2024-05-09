a6f83fa17dffe0568cddef1375ee08d1cb64e328
unknown <kronos888@hotmail.co.uk>
2024-05-09 16:48:59 +0100

    first push

---
+++ Source/AstraVR/AstraGameInstance.cpp
@@ -0,0 +1,132 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AstraGameInstance.h"
+
+
+
+
+
+
+void UAstraGameInstance::NextHair()
+{
+	// Male
+	if (MFIndex == 0)
+	{
+		if (HairIndexMale < (HairListMale.Num() - 1))
+		{
+			HairIndexMale += 1;
+		}		
+	}
+	// Female
+	else
+	{
+		if (HairIndexFemale < (HairListFemale.Num() - 1))
+		{
+			HairIndexFemale += 1;
+		}
+	}
+}
+void UAstraGameInstance::PrevHair()
+{
+	// Male
+	if (MFIndex == 0)
+	{
+		if (HairIndexMale < (HairListMale.Num() - 1))
+		{
+			HairIndexMale -= 1;
+		}
+	}
+	// Female
+	else
+	{
+		if (HairIndexFemale < (HairListFemale.Num() - 1))
+		{
+			HairIndexFemale -= 1;
+		}
+	}
+}
+
+
+
+void UAstraGameInstance::NextOutfit()
+{
+	// Male
+	if (MFIndex == 0)
+	{
+		if (ClothingIndexMale < (ClothingListMale.Num() - 1))
+		{
+			ClothingIndexMale += 1;
+		}
+	}
+	// Female
+	else
+	{
+		if (ClothingIndexFemale < (ClothingListFemale.Num() - 1))
+		{
+			ClothingIndexFemale += 1;
+		}
+	}
+}
+void UAstraGameInstance::PrevOutfit()
+{
+	// Male
+	if (MFIndex == 0)
+	{
+		if (ClothingIndexMale > 0 )
+		{
+			ClothingIndexMale -= 1;
+		}
+	}
+	// Female
+	else
+	{
+		if (ClothingIndexFemale > 0)
+		{
+			ClothingIndexFemale -= 1;
+		}
+	}
+}
+
+
+
+void UAstraGameInstance::SetOutfitIndex(int index)
+{
+	// Male
+	if (MFIndex == 0)
+	{
+		ClothingIndexMale = index;
+	}
+	// Female
+	else
+	{
+		ClothingIndexFemale = index;
+	}
+}
+
+
+void UAstraGameInstance::SetupAvatarMeshData(int& p_mfIndex, TArray <USkeletalMesh*>& p_clothingListFemale, int& p_clothingIndexFemale, TArray <USkeletalMesh*>& p_hairListFemale, int& p_hairIndexFemale, float& p_avatarSizeData, TArray <USkeletalMesh*>& p_clothingListMale, int& p_clothingIndexMale, TArray <USkeletalMesh*>& p_hairListMale, int& p_hairIndexMale)
+{
+	p_mfIndex = MFIndex;
+	p_clothingListFemale = ClothingListFemale;
+	p_clothingIndexFemale = ClothingIndexFemale;
+	p_hairListFemale = HairListFemale;
+	p_hairIndexFemale = HairIndexFemale;
+	p_avatarSizeData = AvatarSizeData;
+	p_clothingListMale = ClothingListMale;
+	p_clothingIndexMale = ClothingIndexMale;
+	p_hairListMale = HairListMale;
+	p_hairIndexMale = HairIndexMale;
+
+}
+
+
+void UAstraGameInstance::SetSkinTone(int& p_mfIndex, TArray <UMaterial*>& p_skinListFemale, int& p_skinIndexFemale, TArray <UMaterial*>& p_skinListMale, int& p_skinIndexMale)
+{
+	p_mfIndex = MFIndex;
+	p_skinListFemale = SkinListFemale;
+	p_skinIndexFemale = SkinIndexFemale;
+	p_skinListMale = SkinListMale;
+	p_skinIndexMale = SkinIndexMale;
+
+}
---
+++ Source/AstraVR/AstraGameInstance.h
@@ -0,0 +1,140 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "AstraGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ASTRAVR_API UAstraGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+	
+	
+public:
+	
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Avatar Configuration")
+	TArray<TSubclassOf<UObject>> AnimBPList;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Avatar Configuration")
+	int MFIndex;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Avatar Configuration")
+	TArray<USkeletalMesh*> ClothingListFemale;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Avatar Configuration")
+	int ClothingIndexFemale;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Avatar Configuration")
+	TArray<USkeletalMesh*> HairListFemale;
+	
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Avatar Configuration")
+	int HairIndexFemale;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Avatar Configuration")
+	float AvatarSizeData;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Avatar Configuration")
+	FText Username;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Avatar Configuration")
+	int Astras;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Avatar Configuration")
+	TArray <USkeletalMesh*> ClothingListMale;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Avatar Configuration")
+	int ClothingIndexMale;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Avatar Configuration")
+	TArray <USkeletalMesh*> HairListMale;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Avatar Configuration")
+	int HairIndexMale;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Avatar Configuration")
+	TArray <UMaterial*> SkinListFemale;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Avatar Configuration")
+	int SkinIndexFemale;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Avatar Configuration")
+	TArray <int> SkinElementListIndexFemale;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Avatar Configuration")
+	bool DidUserPay;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Avatar Configuration")
+	TArray <int> DefaultOutfitIndex;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Avatar Configuration")
+	bool DidUserTryItem;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Avatar Configuration")
+	TArray <UMaterial*> SkinListMale;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Avatar Configuration")
+	int SkinIndexMale;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Avatar Configuration")
+	TArray<TSubclassOf<UObject>> VRAnimBPList;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Avatar Configuration")
+	FString UserID;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Avatar Configuration")
+	FString UserEmail;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Avatar Configuration")
+	FString UserPwd;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Avatar Configuration")
+	FString CompletionTime;
+
+
+
+	// Hair cycling
+	UFUNCTION(BlueprintCallable, Category = "Avatar Configuration")
+	void NextHair();
+	// Hair cycling
+	UFUNCTION(BlueprintCallable, Category = "Avatar Configuration")
+	void PrevHair();
+
+
+	// Outfit cycling
+	UFUNCTION(BlueprintCallable, Category = "Avatar Configuration")
+	void NextOutfit();
+	// Outfit cycling
+	UFUNCTION(BlueprintCallable, Category = "Avatar Configuration")
+	void PrevOutfit();
+
+
+	UFUNCTION(BlueprintCallable, Category = "Avatar Configuration")
+	void SetOutfitIndex(int index);
+
+
+
+	UFUNCTION(BlueprintCallable, Category = "Avatar Configuration")
+	void SetupAvatarMeshData(int& p_mfIndex, TArray <USkeletalMesh*>& p_clothingListFemale, int& p_clothingIndexFemale, TArray <USkeletalMesh*>& p_hairListFemale, int& p_hairIndexFemale, float& p_avatarSizeData, TArray <USkeletalMesh*>& p_clothingListMale, int& p_clothingIndexMale, TArray <USkeletalMesh*>& p_hairListMale, int& p_hairIndexMale);
+
+
+
+	UFUNCTION(BlueprintCallable, Category = "Avatar Configuration")
+	void SetSkinTone(int& p_mfIndex, TArray <UMaterial*>& p_skinListFemale, int& p_skinIndexFemale, TArray <UMaterial*>& p_skinListMale, int& p_skinIndexMale);
+
+
+
+
+
+
+
+
+
+
+
+	
+};
